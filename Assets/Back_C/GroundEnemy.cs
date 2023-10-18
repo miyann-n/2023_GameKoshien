4f4cc3160d9304cd600eba0ae95fc5ea6b824644
DESKTOP-G5M5GKT\kohak <shouyumirin0510@gmail.com>
2023-10-18 23:02:17 +0900

    地上雑魚敵の挙動が完成しました。

--- Assets/Back_C/GroundEnemy.cs
+++ Assets/Back_C/GroundEnemy.cs
@@ -20,7 +20,23 @@ public class GroundEnemy : MonoBehaviour
     //プレイヤーがエリア内に入ったかを示すグローバル変数
     bool AreaChecker;
     
-    
+    // 自分自身
+    [SerializeField] private Transform _self;
+
+    // ターゲット
+    [SerializeField] private Transform _target;
+
+    // 視野角（度数法）
+    [SerializeField] private float _sightAngle;
+
+    // 視界の最大距離
+    [SerializeField] private float _maxDistance = float.PositiveInfinity;
+
+    #region Logic*/
+
+    /// <summary>
+    /// ターゲットが見えているかどうか
+    /// </summary>
     // Start is called before the first frame update
 
     void Start()
@@ -33,8 +49,8 @@ public class GroundEnemy : MonoBehaviour
     void Update()
     {   
         Vector2 EnemyPosition =this.transform.position;
-        Debug.Log("Enemy x = " + EnemyPosition.x);
-        Debug.Log("Enemy y = " + EnemyPosition.y);
+        //Debug.Log("Enemy x = " + EnemyPosition.x);
+        //Debug.Log("Enemy y = " + EnemyPosition.y);
 
         if ( Check == false){
             if( EnemyPosition.x == EnemyArea ){
@@ -56,7 +72,7 @@ public class GroundEnemy : MonoBehaviour
 void RigidbodyAccelerator()
 {
     // 加速度を指定してRigidbodyに力を加える
-   // _rigidbody.AddForce(acceleration, ForceMode2D.Acceleration);
+    _rigidbody.AddForce(acceleration, ForceMode2D.Force);
 }
 
 /*攻撃処理*/
@@ -64,12 +80,12 @@ void RigidbodyAccelerator()
     {
         yield return new WaitForSeconds(0.5f); // 0.5秒待機
         Vector2 PlayerPosition =GameObject.Find("Player").transform.position;
-        Debug.Log("Player x = " + PlayerPosition.x);
-        Debug.Log("Player y = " + PlayerPosition.y);
+        //Debug.Log("Player x = " + PlayerPosition.x);
+        //Debug.Log("Player y = " + PlayerPosition.y);
 
         Vector2 EnemyPosition =this.transform.position;
-        Debug.Log("Enemy x = " + EnemyPosition.x);
-        Debug.Log("Enemy y = " + EnemyPosition.y);
+        //Debug.Log("Enemy x = " + EnemyPosition.x);
+        //Debug.Log("Enemy y = " + EnemyPosition.y);
 
         
         float Dist = Vector2.Distance(PlayerPosition,EnemyPosition);
@@ -83,15 +99,17 @@ void RigidbodyAccelerator()
 /*視界に入ったかの判定するやつ*/
 
 /*プレイヤーが視界に入ったかどうかの判定*/
-/*void PlayerChecker(){
-    if(IsVisible == true){
+void PlayerChecker(){
+    var isVisible = IsVisible();
+    Debug.Log(isVisible);
+    /*if(IsVisible == true){
         Check = true;
     }
     else{
         Check = false;
-    }
+    }*/
 }
-*/
+
 /*プレイヤーがエリアにはいったかどうかの判定*/
 void OnCollisionStay2D(Collision2D col) {
     if (col.gameObject.name == "Player")
@@ -103,28 +121,11 @@ void OnCollisionStay2D(Collision2D col) {
         AreaChecker = false;
      }
     }
-}
 
-/*class SightCheckerExample2D : MonoBehaviour
-{
-    // 自分自身
-    [SerializeField] private Transform _self;
-
-    // ターゲット
-    [SerializeField] private Transform _target;
-
-    // 視野角（度数法）
-    [SerializeField] private float _sightAngle;
-
-    // 視界の最大距離
-    [SerializeField] private float _maxDistance = float.PositiveInfinity;
-
-    #region Logic*/
-
-    /// <summary>
-    /// ターゲットが見えているかどうか
-    /// </summary>
-    /*public bool IsVisible()
+//public class SightChecker : MonoBehaviour
+//{
+    
+    public bool IsVisible()
     {
         // 自身の位置
         Vector2 selfPos = _self.position;
@@ -157,5 +158,6 @@ void OnCollisionStay2D(Collision2D col) {
     // 視界判定の結果をGUI出力
     
     #endregion
-    
-}*/
+    }
+//}
+
