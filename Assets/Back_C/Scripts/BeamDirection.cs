13e4479dcf4cd5a865b02fbeafac1de285648037
宮内七菜 <>
2023-10-26 09:32:06 +0900

    空中敵のビームをプレイヤーに向けた

---
+++ Assets/Back_C/Scripts/BeamDirection.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamDirection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = GameObject.FindWithTag("Player").transform.position;
+        Vector3 dir = targetPos - transform.position;
+        // ベクトルの方向を回転角度に変換します
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        // オブジェクトを指定した角度に回転させます
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        Debug.Log(angle);
+    }
+}
