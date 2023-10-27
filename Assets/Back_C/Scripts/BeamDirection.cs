851fd2a31195895999b2c598e82837353f99df45
宮内七菜 <>
2023-10-27 21:14:19 +0900

    雑魚敵３種類完成(死亡処理はプレイヤー攻撃との兼ね合いで作成してない)

--- Assets/Back_C/Scripts/BeamDirection.cs
+++ Assets/Back_C/Scripts/BeamDirection.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class BeamDirection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private bool Tracking;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        Vector3 targetPos = GameObject.FindWithTag("Player").transform.position;
-        Vector3 dir = targetPos - transform.position;
-        // ベクトルの方向を回転角度に変換します
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        // オブジェクトを指定した角度に回転させます
-        transform.rotation = Quaternion.AngleAxis(angle + 90f, Vector3.forward);
+        if(Tracking == true)
+        {
+            Vector3 targetPos = GameObject.FindWithTag("Player").transform.position;
+            Vector3 dir = targetPos - transform.position;
+            // ベクトルの方向を回転角度に変換します
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            // オブジェクトを指定した角度に回転させます
+            transform.rotation = Quaternion.AngleAxis(angle + 90f, Vector3.forward);
+        }    
+        
+    }
+    public void StartDirection()
+    {
+        Tracking = false;
+        if(Tracking == false)
+        {
+            Vector3 targetPos = GameObject.FindWithTag("Player").transform.position;
+            Vector3 dir = targetPos - transform.position;
+            // ベクトルの方向を回転角度に変換します
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            // オブジェクトを指定した角度に回転させます
+            transform.rotation = Quaternion.AngleAxis(angle + 90f, Vector3.forward);
+        }
+        StartCoroutine("Wait");
+    }
+     IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Tracking = true;
     }
 }
