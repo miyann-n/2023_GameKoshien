41f46ce1e6c691899fd7ba82552d9e99ab652aad
高尾拓幹 <bs222039@sun.ac.jp>
2023-10-31 16:37:26 +0900

    試行錯誤

--- Assets/Back_B/Scripts/Energy.cs
+++ Assets/Back_B/Scripts/Energy.cs
@@ -2,26 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
-
 public class Energy : MonoBehaviour
 {
 
-    public int maxEnergy; //最大のエネルギー
+    public int maxEnergy = 100; //最大のエネルギー
     public float currentEnergy; //現在のエネルギー
-    public bool isMoveObjectSet; //物体移動のアビリティが選択されているかどうか
-    public bool isGravityIncreaseSet; //重力増強のアビリティが選択されているかどうか
-    public bool isGravityInversionSet; //重力反転のアビリティが選択されているかどうか
+    public bool isMoveObjectSet = false; //物体移動のアビリティが選択されているかどうか
+    public bool isGravityIncreaseSet = false; //重力増強のアビリティが選択されているかどうか
+    public bool isGravityInversionSet = false; //重力反転のアビリティが選択されているかどうか
     private float energyIncreaseDelay = 3f; // エネルギー増加の遅延時間
     private float energyIncreaseRate = 20f; // エネルギー増加の速度
     private float energyIncreaseTimer = 0f; // エネルギー増加のタイマー
 
-    void Start()
+    private void Start()
     {
-        maxEnergy = 100;
-        currentEnergy = maxEnergy;  
-        isMoveObjectSet = false;
-        isGravityIncreaseSet = false;
-        isGravityInversionSet = false;
+        currentEnergy = maxEnergy;
     }
     void Update()
     {
--- Assets/Front_A/Ability.cs
+++ Assets/Front_A/Ability.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class Ability : MonoBehaviour
 {
-
     public Energy energy;
     public Sprite newSpriteMove;
     public Sprite newSpriteJump;
