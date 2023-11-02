586c49bf37b66a548ee39c94260772a8f58351be
黒川龍雅 <136037557+RyugaKurokawa@users.noreply.github.com>
2023-11-02 14:26:32 +0900

    瓦礫挙動完成して一応テスト環境での動作確認まで済みました

--- Assets/Back_A/RubbleAttack/BigRubble.cs
+++ Assets/Back_A/RubbleAttack/BigRubble.cs
@@ -6,63 +6,87 @@ public class BigRubble : MonoBehaviour
 {
     public bool isCheckLeftClick;
     public bool isCheckMousePointLR;
-    public GravityClickCharactor gClickcharacktor;
+    private bool isCheckCollisionPlayer;
+    Rigidbody2D rb;
+    
+    public Player player;
+
     // Start is called before the first frame update
     void Start()
     {
         isCheckLeftClick = false;
         isCheckMousePointLR = false;
+        isCheckCollisionPlayer = false;
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isCheckLeftClick){
-             GameObject player = GameObject.FindWithTag("Player");
-             Vector2 playerPosition = player.transform.position;
-             Vector2 rubblePosition = this.transform.position;
-             Debug.Log("プレイヤー取得");
-             rubblePosition = playerPosition;
-             this.transform.position = rubblePosition;
-             Vector2 mousePosition = Input.mousePosition;
-             Vector2 worldPos = Camera.main.ScreenToWorldPoint(new Vector2(mousePosition.x,mousePosition.y));
-             Debug.Log(playerPosition);
-             if(worldPos.x > playerPosition.x){
+        if(Input.GetMouseButton(0) && player.isCheckBigObjectMove){
+            RubbleMove();
+        }
+
+        if(Input.GetMouseButtonUp(0) && player.isCheckBigObjectMove){
+            RubbleLaunch();
+        }
+
+    }
+
+    private void RubbleMove(){
+        if(isCheckCollisionPlayer){
+            GameObject player = GameObject.FindWithTag("Player");
+            Vector2 playerPosition = player.transform.position;
+            this.transform.position = new Vector2(playerPosition.x+1.2f,this.transform.position.y);
+            Debug.Log("プレイヤー取得");
+            Vector2 mousePosition = Input.mousePosition;
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(new Vector2(mousePosition.x,mousePosition.y));
+            Debug.Log(playerPosition);
+            isCheckLeftClick = true;
+
+            if(worldPos.x > playerPosition.x){
                 isCheckMousePointLR = true;
                 Debug.Log("右");
-             }
-             else if(worldPos.x < playerPosition.x){
+                this.transform.position = new Vector2(playerPosition.x+2.2f,0.15f);
+            }
+            else if(worldPos.x < playerPosition.x){
                 isCheckMousePointLR = false;
                 Debug.Log("左");
-             }
+                this.transform.position = new Vector2(playerPosition.x-2.2f,0.15f);
+            }
+
         }
     }
 
-    public void OnMouseUp(){
-        
-        if(gClickcharacktor.isCheckCanBigObject){
+    public void RubbleLaunch(){
+        if(isCheckLeftClick){
             Debug.Log("クリック離し");
             isCheckLeftClick = false;
+            
             if(isCheckMousePointLR){
-                Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
-                Vector2 force = new Vector2(100f,50f);
+                Vector2 force = new Vector2(50f,14f);
                 Debug.Log("右に飛ばす");
                 rb.AddForce (force, ForceMode2D.Impulse);   
             }
             else{
-                Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
-                Vector2 force = new Vector2(-100f,5f);
+                Vector2 force = new Vector2(-50f,14f);
                 Debug.Log("左に飛ばす");
                 rb.AddForce (force, ForceMode2D.Impulse);               
             }
         }
-
     }
 
-    public void OnMouseDrag(){
-        if(gClickcharacktor.isCheckCanBigObject){
-            isCheckLeftClick = true;
-            Debug.Log("クリック確認");
+    private void OnTriggerStay2D(Collider2D collider) {
+        if(collider.gameObject.tag == "Player"){
+            isCheckCollisionPlayer = true;
+            Debug.Log("Collision!");
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D collider) {
+        if(collider.gameObject.tag == "Player"){
+            isCheckCollisionPlayer = false;
+            Debug.Log("Exit");
         }
     }
 
